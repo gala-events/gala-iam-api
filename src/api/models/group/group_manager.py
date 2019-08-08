bd1cc0d3e721545848bdf0c40d396b8fb6085828
Avik Sarkar <sayavik@gmail.com>
2019-08-09 01:58:42 +0530

    Support for search across multiple fields now supported also added moe validation across new group creation

--- src/api/models/group/group_manager.py
+++ src/api/models/group/group_manager.py
@@ -28,10 +28,14 @@ class GroupManager(BaseRecordManager):
         for subject in new_group.subjects:
             subject_kind = subject.kind
             if subject_kind == "USER":
-                UserManager.find_by_name(db, subject.name)
+                if len(UserManager.find_by_name(db, subject.name)) is not 1:
+                    raise ValidationError(
+                        "User [%s] does not exist" % subject.name)
 
             elif subject_kind == "SERVICE_ACCOUNT":
-                ServiceAccountManager.find_by_name(db, subject.name)
+                if len(ServiceAccountManager.find_by_name(db, subject.name)) is not 1:
+                    raise ValidationError(
+                        "Service Account [%s] does not exist" % subject.name)
 
             else:
                 raise ValidationError(
@@ -49,6 +53,11 @@ class GroupManager(BaseRecordManager):
         Returns:
             Group -- newly created group
         """
+        existing_group = GroupManager.find_by_name(db, record.metadata.name)
+        if existing_group:
+            raise ValidationError(
+                "Group with name [%s] already exists" % record.metadata.name)
+
         cls.validate_group(db, record)
         return super(GroupManager, cls).create(db, record)
 
@@ -64,7 +73,11 @@ class GroupManager(BaseRecordManager):
         Returns:
             BaseRecord -- Updated record
         """
-        cls.find_by_uuid(db, record_uuid)
+        existing_group = cls.find_by_uuid(db, record_uuid)
         updated_record = cls.model(**record.dict(), uuid=record_uuid)
+        if updated_record.metadata.name != existing_group.metadata.name:
+            if GroupManager.find_by_name(db, updated_record.metadata.name):
+                raise ValidationError(
+                    "Group with name [%s] already exists" % record.metadata.name)
         cls.validate_group(db, updated_record)
         return super(GroupManager, cls).update(db, record_uuid, record)
